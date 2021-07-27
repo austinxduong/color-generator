adb647c921dfb47f72b530b14e6049138adc0c0c
austinxduong <axduong2@gmail.com>
2021-07-26 17:28:54 -0700

    passMouseMove into presentation Container

--- src/App.js
+++ src/App.js
@@ -46,6 +46,8 @@ export default class Color extends Component  {
               style={{
                 backgroundColor: this.state.bgColor
               }}
+              onMouseMove={this.MouseHover}
+              className="App"
             />
           </div>
         </>
