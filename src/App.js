36e93735943d936a4d89cdcbb304ae26a4f6b234
austinxduong <axduong2@gmail.com>
2021-07-26 17:13:53 -0700

    add clickHandler + pass in presentation container

--- src/App.js
+++ src/App.js
@@ -1,8 +1,53 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import randomColor from "randomcolor";
+import randomColor from 'randomcolor';
 
-import copy from 'clipboard-copy'
+import copy from 'clipboard-copy';
 
-import "./styles.css";
+import './styles.css';
+
+export default class Color extends Component  {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bgColor: '',
+      display:false
+    };
+  }
+
+    clickHandler=(event) => {
+      copy(this.state.bgColor);
+
+      this.setState({ display:true });
+    }
+
+    clickHideHandler=(event) => {
+      this.setState({ display:false });
+    }
+
+    render() {
+      return (
+        <>
+          <div onClick={this.clickHideHandler} style={{ display: this.state.display ? '' : 'none', backgroundColor:'black', height: '30px', width:'100%' }}>
+            <h5 style ={{ color:'white', cursor: 'pointer', margin:'auto 0px' }}> Here's the color you picked! We copied it for you. </h5>
+          </div>
+
+          <div className="divOuter">
+            <div
+              onClick={this.clickHandler}
+              style={{
+                backgroundColor: this.state.bgColor
+              }}
+            />
+          </div>
+        </>
+      );
+    }
+
+}
+
+
+
+
+
 
