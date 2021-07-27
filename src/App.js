bbca2d2cb7522368f8682ed995bed6c1684a3c94
austinxduong <axduong2@gmail.com>
2021-07-26 17:23:50 -0700

    add MouseHover function in container component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React, { Component } from 'react';
 
 import randomColor from 'randomcolor';
@@ -25,11 +26,18 @@ export default class Color extends Component  {
       this.setState({ display:false });
     }
 
+    MouseHover = e => {
+      const color = randomColor();
+      this.setState({
+        bgColor: color
+      });
+    };
+
     render() {
       return (
         <>
           <div onClick={this.clickHideHandler} style={{ display: this.state.display ? '' : 'none', backgroundColor:'black', height: '30px', width:'100%' }}>
-            <h5 style ={{ color:'white', cursor: 'pointer', margin:'auto 0px' }}> Here's the color you picked! We copied it for you. </h5>
+            <h5 style ={{ color:'white', cursor: 'pointer', margin:'auto 0px' }}> Here is the color you picked! We copied it for you. </h5>
           </div>
 
           <div className="divOuter">
